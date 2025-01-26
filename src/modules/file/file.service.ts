1680656bf44e2b71eef03dc6e52e691a1749605f
nmthangdn2000 <uptimumdn2000@gmail.com>
2025-01-27 00:09:11 +0700

    fix api file

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import { Prisma } from "@prisma/client";
-import { prismaClient } from "../../core/middlewares/database.middleware";
-import { TFileCreateSchema } from "./schema/file-create.schema";
-import { TFileFilterSchema } from "./schema/file-get.schema";
-
-class FileService {
-  async create(data: TFileCreateSchema, userId: string) {
-    const file = await prismaClient().file.create({
-      data: {
-        ...data,
-        userId,
-      },
-    });
-
-    return file;
-  }
-
-  async getAll(filter: TFileFilterSchema) {
-    const where: Prisma.FileWhereInput = {};
-
-    if (filter.name) {
-      where.OR = [
-        {
-          name: {
-            contains: filter.name,
-          },
-        },
-      ];
-    }
-
-    const files = await prismaClient().file.findMany({
-      where,
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-
-    return files;
-  }
-=======
 import { TUserUpdateSchema } from "./schema/file-update.schema";
 
 class FileService {
@@ -56,7 +15,6 @@ class FileService {
   }
 
   async delete(id: string, input: TUserUpdateSchema) {}
->>>>>>> 4c4fbbb9841da73830f11b3d37880285ac426c4f
 }
 
 export default new FileService();
